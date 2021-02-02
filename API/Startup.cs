ef43740a1363e6899db8259b9d3de495b7d06418
vasile.moraru <chisinau2000@gmail.com>
2021-02-02 18:35:24 +0200

    angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,19 +32,13 @@ namespace API
             {
                 optionBuilder.UseSqlServer(Configuration.GetConnectionString("DbConnection"));
             });
-            
-            
-            
-            
-            
-            
-            
+
+
+            services.AddCors();
+
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,13 +54,11 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
